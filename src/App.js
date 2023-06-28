64f28bb9f3c7f75b09199d712cd1a5038781a699
Wen Ni Lim <wenni1999@gmail.com>
2023-06-28 16:40:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,143 @@
-import React from 'react';
 import {
+  Button,
   ChakraProvider,
-  Box,
+  Flex,
+  Input,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import React, { useEffect, useState } from 'react';
+import { EditIcon, DeleteIcon } from '@chakra-ui/icons';
+import { addTodo, deleteTodo, editTodo, getAllTodos } from './utils/api';
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState('');
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [todoId, setTodoId] = useState('');
+  const [displayLimit, setDisplayLimit] = useState(7);
+
+  useEffect(() => {
+    getAllTodos(setTodo);
+  }, []);
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true);
+    setTodoId(_id);
+    setText(text);
+  };
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
+      <Flex
+        justifyContent={'center'}
+        alignItems={'center'}
+        bgImg={{
+          base: 'none',
+          md: 'https://freesvg.org/img/laurent_panda_point_d_interrogation.png',
+        }}
+        bgRepeat={'no-repeat'}
+        bgSize={'300px'}
+        bgPosition={'bottom left'}
+        w={'100%'}
+      >
+        <Flex
+          flexDirection={'column'}
+          padding={5}
+          justifyContent={'center'}
+          alignItems={'center'}
+          w={'100%'}
+        >
+          <Text fontSize={'3xl'} fontWeight={800}>
+            What To Do?
+          </Text>
+          <Flex py={5}>
+            <Input
+              marginX={5}
+              focusBorderColor="black"
+              variant="flushed"
+              placeholder="Add ToDos..."
+              value={text}
+              onChange={e => setText(e.target.value)}
+              onKeyDown={e => {
+                if (e.key === 'Enter' && text !== '') {
+                  isUpdating
+                    ? editTodo(todoId, text, setText, setTodo, setIsUpdating)
+                    : addTodo(text, setText, setTodo);
+                }
+              }}
+            />
+            <Button
+              shadow={'2xl'}
+              isDisabled={text === '' ? true : false}
+              onClick={() =>
+                isUpdating
+                  ? editTodo(todoId, text, setText, setTodo, setIsUpdating)
+                  : addTodo(text, setText, setTodo)
+              }
+              variant="outline"
+              colorScheme="black"
             >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+              {isUpdating ? 'Update' : 'Add'}
+            </Button>
+          </Flex>
+          {todo.slice(0, displayLimit).map(item => (
+            <List
+              key={item._id}
+              content={item.text}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteMode={() => deleteTodo(item._id, setTodo)}
+            />
+          ))}
+          {todo.length > displayLimit && (
+            <Button
+              onClick={() => setDisplayLimit(todo.length)}
+              variant="outline"
+              colorScheme="black"
+              mt={2}
+              w={'300px'}
+            >
+              Load More...
+            </Button>
+          )}
+          {displayLimit > 7 && (
+            <Button
+              onClick={() => setDisplayLimit(7)}
+              variant="outline"
+              colorScheme="black"
+              mt={2}
+              w={'300px'}
+            >
+              Collapse
+            </Button>
+          )}
+        </Flex>
+      </Flex>
     </ChakraProvider>
+  );
+}
+
+function List({ content, updateMode, deleteMode }) {
+  return (
+    <Flex
+      justifyContent={'space-between'}
+      borderRadius={'md'}
+      marginY={'2'}
+      borderColor={'white'}
+      bg="black"
+      minWidth={'290px'}
+      minH={'50px'}
+      p={4}
+      color="white"
+      whiteSpace={'normal'}
+      shadow={'2xl'}
+    >
+      {content}
+      <Flex alignItems={'center'}>
+        <EditIcon mr={1} cursor={'pointer'} onClick={updateMode} />
+        <DeleteIcon ml={1} cursor={'pointer'} onClick={deleteMode} />
+      </Flex>
+    </Flex>
   );
 }
 
---
+++ src/utils/api.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const baseUrl = 'http://192.168.0.145:8000';
+
+const getAllTodos = async setTodo => {
+  try {
+    const response = await axios.get(baseUrl);
+    setTodo(response.data);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const addTodo = async (text, setText, setTodo) => {
+    try {
+      const response = await axios.post(`${baseUrl}/add`, { text });
+      console.log(response.data);
+      setText('');
+      await getAllTodos(setTodo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+const editTodo = async (todoId, text, setText, setTodo, setIsUpdating) => {
+    try {
+      const response = await axios.put(`${baseUrl}/update`, { _id: todoId, text });
+      console.log(response.data);
+      setText('');
+      getAllTodos(setTodo);
+      setIsUpdating(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+const deleteTodo = async (_id, setTodo) => {
+  try {
+    const response = await axios.delete(`${baseUrl}/delete`, { data: { _id } });
+    console.log('🧩', response.data);
+    getAllTodos(setTodo);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getAllTodos, addTodo, editTodo, deleteTodo };
